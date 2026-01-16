ceec244f236ceec696f25711ccfca724229cca3d
meditation <jaro@gmail.com>
2026-01-16 14:30:50 +0300

    done

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const API = "http://localhost:5000/api/votes";
 
-function vote(option) {
-  fetch(API, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ option })
-  }).then(loadResults);
-}
 
-function loadResults() {
   fetch(API)
     .then(res => res.json())
     .then(data => {
