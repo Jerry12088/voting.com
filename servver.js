571753dba335519a6f1ec12ad190f898577f62a1
meditation <jaro@gmail.com>
2026-01-16 11:11:12 +0300

    added afew line

--- servver.js
+++ servver.js
@@ -17,3 +17,6 @@ app.use("/api/votes", voteRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
