51aba0b9d10940d4f00011bb6941f6e4d093096a
meditation <jaro@gmail.com>
2026-01-20 10:27:47 +0300

    eevert "modified servver.js"

--- servver.js
+++ servver.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/votingDB")
-  .then(() => console.log("MongoDB Connectedd"))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.use("/api/votes", voteRoutes);
